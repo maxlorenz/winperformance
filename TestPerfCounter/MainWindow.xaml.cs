f062ab20a7e860f78af10e24a201cbae903fbda0
maxlorenz <max-lorenz@gmx.net>
2014-08-15 16:31:58 +0200

    Small update

---
+++ TestPerfCounter/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TestPerfCounter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PerformanceMetric[] metrics;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /*
+             * Performance measured by querying the WMI.
+             * See here for additional info
+             * http://serverfault.com/questions/54753/common-wql-monitoring-queries
+             * 
+             */
+
+            metrics = new PerformanceMetric[] {
+                new PerformanceMetric("select ProcessorQueueLength from Win32_PerfFormattedData_PerfOS_System", 
+                    10, txtProcessorQueueLength, pgrProcessorQueueLength),
+                new PerformanceMetric("select PagesInputPerSec from Win32_PerfFormattedData_PerfOS_Memory", 
+                    0, txtPagesInputPerSec, pgrPagesInputPerSec),
+                new PerformanceMetric("select AvgDiskQueueLength from Win32_PerfFormattedData_PerfDisk_PhysicalDisk where name = '_Total'", 
+                    2, txtAverageDiskQueueLength, pgrAverageDiskQueueLength),
+                new PerformanceMetric("select OutputQueueLength from Win32_PerfFormattedData_Tcpip_NetworkInterface", 
+                    0, txtOutputQueueLength, pgrOutputQueueLength),
+                new PerformanceMetric("select PacketsReceivedErrors from Win32_PerfFormattedData_Tcpip_NetworkInterface", 
+                    0, txtPacketsReceivedErrors, pgrPacketsReceivedErrors)
+            };
+
+            Thread perfCounter = new Thread(new ThreadStart(perfCount));
+            perfCounter.IsBackground = true;
+            perfCounter.Start();
+        }
+
+        public void perfCount()
+        {
+            while (true)
+            {
+                foreach (PerformanceMetric pm in metrics)
+                {
+                    pm.Update();
+                }
+            }
+        }
+    }
+}
---
+++ TestPerfCounter/PerformanceMetric.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TestPerfCounter
+{
+    class PerformanceMetric
+    {
+        public string query;
+        public int maxValue;
+        public Label label;
+        public ProgressBar progress;
+
+        private WMIConnector wc;
+
+        public PerformanceMetric(string query, int maxValue, Label label, ProgressBar progress)
+        {
+            this.query = query;
+            this.maxValue = maxValue;
+            this.label = label;
+            this.progress = progress;
+
+            this.wc = new WMIConnector();
+        }
+
+        public void Update()
+        {
+            string wmiQueryResult = wc.MakeQuery(this.query)[0];
+            giveColorFeedback(int.Parse(wmiQueryResult) <= maxValue);
+
+            this.label.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                label.Content = wmiQueryResult;
+                progress.Value = int.Parse(wmiQueryResult);
+            }));
+        }
+
+        private void giveColorFeedback(bool healthy)
+        {
+            Brush color = healthy ? Brushes.LimeGreen : Brushes.OrangeRed;
+
+            label.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                label.Background = color;
+                progress.Foreground = color;
+            }));
+        }
+
+    }
+}
